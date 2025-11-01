8020cf3d26cd99750fa25d566cadd38d319dbe89
Thomas Röggla <t.roggla@cwi.nl>
2025-11-01 20:55:16 +0100

    added basic setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="amplify-portable-interface",
+    packages=find_packages(),
+    version="1.0.0",
+    description="Python interface library for AMPLIFY Portable",
+    author="Thomas Röggla",
+)
