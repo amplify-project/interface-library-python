3ae56e27978d62630d05515ca3db0d4c524d174f
Thomas Röggla <t.roggla@cwi.nl>
2025-11-27 17:22:19 +0100

    added types for specifying stream properties

--- amplify-portable-interface/stream.py
+++ amplify-portable-interface/stream.py
@@ -1,5 +1,17 @@
+from typing import Literal, Union
+
 from connection import Connection
 
+StreamType = Union[
+    Literal["discrete"],
+    Literal["continuous"]
+]
+
+StreamDataType = Union[
+    Literal["number"],
+    Literal["string"],
+    Literal["boolean"],
+]
 
 class Stream(object):
     def __init__(self, connection: Connection, name: str) -> None:
