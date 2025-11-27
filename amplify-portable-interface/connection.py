d92f85a25641bf5226d076cd046da9f52100c7be
Thomas Röggla <t.roggla@cwi.nl>
2025-11-27 18:01:37 +0100

    added method to retrieve all streams from the server to Connection

--- amplify-portable-interface/connection.py
+++ amplify-portable-interface/connection.py
@@ -1,6 +1,6 @@
 import json
 from redis import Redis
-from typing import List, Literal, Union
+from typing import Dict, List, Literal, Union
 
 from stream import Stream, StreamType, StreamDataType
 
@@ -14,24 +14,41 @@ class Connection(object):
     __SERVICE_REGISTRY = "available_streams"
 
     __redis: Redis
-    __streams: List[Stream]
+    __registered_streams: List[Stream]
     __type: ConnectionType
 
     def __init__(self, host: str, port: int, type: ConnectionType) -> None:
         self.__redis = Redis(host, port)
         self.__type = type
-        self.__streams = []
+        self.__registered_streams = []
 
     def close(self):
         self.__redis.close()
 
     def register_stream(self, name: str, type: StreamType, data_type: StreamDataType) -> Stream:
         stream = Stream(self, name, type, data_type)
-        self.__streams.append(stream)
+        self.__registered_streams.append(stream)
 
-        self.__redis.set(
+        self.__redis.hset(
             self.__SERVICE_REGISTRY,
+            stream.name,
             json.dumps(stream.serialize())
         )
 
         return stream
+
+    def get_available_streams(self) -> Dict[str, Stream]:
+        stream_properties = self.__redis.hgetall(self.__SERVICE_REGISTRY)
+        streams = {}
+
+        for name, value in stream_properties.items():
+            stream_info = json.loads(value)
+
+            streams[name] = Stream(
+                self,
+                name,
+                stream_info["type"],
+                stream_info["dataType"]
+            )
+
+        return streams
