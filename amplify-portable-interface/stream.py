8ff643fa49a0dea3b9793a65d272afb550d23d3b
Thomas Röggla <t.roggla@cwi.nl>
2025-11-27 17:23:29 +0100

    added additional properties to Stream constructor

--- amplify-portable-interface/stream.py
+++ amplify-portable-interface/stream.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from typing import Literal, Union
 
 from connection import Connection
@@ -13,10 +14,13 @@ StreamDataType = Union[
     Literal["boolean"],
 ]
 
-class Stream(object):
-    def __init__(self, connection: Connection, name: str) -> None:
+class Stream(Serializable):
+    def __init__(self, connection: Connection, name: str, type: StreamType, data_type: StreamDataType) -> None:
         self.__name = name
         self.__connection = connection
+
+        self.__type = type
+        self.__data_type = data_type
 
     def publish(self) -> None:
         pass
